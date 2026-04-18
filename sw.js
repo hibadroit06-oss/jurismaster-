47508509c91e640f445c0d5e6ee0f99246f30a0e
hibadroit06-oss <hiba.droit06@gmail.com>
2026-04-19 01:37:08 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,16 @@
 self.addEventListener('fetch', e => {});
+
+self.addEventListener('notificationclick', e => {
+  e.notification.close();
+  e.waitUntil(clients.openWindow('/jurismaster-/'));
+});
+
+self.addEventListener('message', e => {
+  if (e.data && e.data.type === 'SHOW_NOTIF') {
+    self.registration.showNotification(e.data.title, {
+      body: e.data.body,
+      tag: 'jurismaster-daily',
+      renotify: true
+    });
+  }
+});
